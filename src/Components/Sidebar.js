c8ee281d68fe81b905f8d10a7ffb22ad25111b05
Ryan Mendoza <71746816+ryan-mendoza@users.noreply.github.com>
2021-07-22 00:28:23 -0700

    sidebar component added

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+function Sidebar() {
+    return (
+        <div className="Sidebar">
+            {/* <h1>This is the sidebar</h1> */}
+        </div>
+    )
+}
+
+export default Sidebar
